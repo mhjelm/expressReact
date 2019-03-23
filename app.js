64af251a884fd1e23b590e2a163006180719950c
mhjelm <maghje@gmail.com>
2019-03-23 19:24:36 +0100

    change

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  console.log('request');
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/test', testRouter);
