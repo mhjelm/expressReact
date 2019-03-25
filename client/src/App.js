807b36a5f488470414b73e57cf6b87d30e5a6bf7
mhjelm <maghje@gmail.com>
2019-03-25 23:22:34 +0100

    btn

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,30 @@ import './App.css';
 
 class App extends Component {
 
+  constructor()
+  {
+    super();
+
+    this.state = {
+      data: 12
+    }
+  }
+
   getData() {
-    console.log(33);
 
     fetch('api/foo', {
         method: 'post'
       })
       .then(resp => resp.json())
-      .then(data => console.log(data));
+      .then(data => {
+        console.log(data);
+        this.setState({data: data.data});
+      });
+  }
+
+  onTextChange(e)
+  {
+    this.setState({data: e.target.value});  
   }
 
   render() {
@@ -18,6 +34,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         <button onClick={e => this.getData()}>get data</button>
+        <textarea name="" id="" cols="30" rows="10" onChange={this.onTextChange.bind(this)}
+          value={this.state.data}>
+          </textarea>
         </header>
       </div>
     );
